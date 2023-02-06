3dacaac6718b92b9c371c8f689275d0f19bed3e6
Mark Berkey <111755911+mbrky6@users.noreply.github.com>
2023-02-06 01:46:07 +0000

    Added more methods to make use of imports and modules

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-const xyz = require('./people');
+// Import from the specified file
+const {people, ages} = require('./people');
+
+console.log(people, ages);
+
+// Import the native os module
+const os = require("os");
+
+// Print the OS platform and home directory
+console.log(os.platform(), os.homedir());
--- people.js
+++ people.js
@@ -1,3 +1,10 @@
 const people = ["Q", "Sal", "Murr", "Joe"];
+const ages = [46, 46, 46, 46]; // Didn't think this one through
 
 console.log(people);
+
+// Export the values to make them available from other places
+module.exports = {
+  people,
+  ages
+};
